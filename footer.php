c3c99658393e52d470ec4563948035a5da9e7efd
Filip-Konkowski <fkonkowski@gmail.com>
2015-10-23 14:24:12 +0200

    added new files header and footer

---
+++ footer.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: filip
+ * Date: 23.10.15
+ * Time: 13:19
+ */
---
+++ header.php
@@ -0,0 +1,27 @@
+<?php
+?>
+
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" id="E-shop" href="#">E-shop</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li id="register"><a href="register.php">Register</a></li>
+            </ul>
+            <form class="navbar-form navbar-left" role="search">
+                <div class="form-group">
+                    <input type="text" class="form-control" id="email" placeholder="email">
+                </div>
+                <div class="form-group">
+                    <input type="text" class="form-control" id="password" placeholder="password">
+                </div>
+                <button type="submit" class="btn btn-default" id="login">Login</button>
+            </form>
+
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
