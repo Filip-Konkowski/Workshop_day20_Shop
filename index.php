6d3af02a25a646b87725f0edb980006db3a04dda
Filip-Konkowski <fkonkowski@gmail.com>
2015-10-23 14:13:19 +0200

    added header and Altorouter

--- index.php
+++ index.php
@@ -11,3 +11,35 @@ if($db->connect_error){
 else{
     echo 'polaczenie udane';
 }
+
+$router = new AltoRouter();
+$router->setBasePath(BASE_PATH);
+require_once ("routing.php");
+$match = $router->match()
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>E-shop</title>
+    <link href="css/css_bootstrap/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<?php
+require_once("header.php");
+if ($match == true){
+    require_once ($match["target"]);
+}
+require_once("footer.php");
+
+?>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</body>
+</html>
+
