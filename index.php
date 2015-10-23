15fc7fdc6def3ebf5051557c82cfd6c8c3660bdc
Washkiesky <daniel.waszkiewicz@gmail.com>
2015-10-23 11:08:47 +0200

    dodanie polaczenia do pliku

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 
 require_once __DIR__ . "/vendor/autoload.php";
+include ('config/db.php');
+
+$db = new mysqli(DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DB);
+
+if($db->connect_error){
+    die('nieudane. blad:' .  $db->connect_error);
+}
+else{
+    echo 'polaczenie udane';
+}
