49b1f954758dcfbdb7208b9a9165b5baa36d9865
Filip-Konkowski <fkonkowski@gmail.com>
2015-10-24 11:55:24 +0200

    first adding items with photosPath to DB and moving multiple files to photo folder

--- addItem.php
+++ addItem.php
@@ -10,6 +10,22 @@ if(!$userAdmin){ // TODO Jeżeli nie jest admin to wraca do index
 if($_SERVER["REQUEST_METHOD"] === "POST") {
     Item::setConnection($db);
     $newItem = Item::addItem($_POST["itemName"], $_POST["itemPrice"], $_POST["itemDescription"], $_POST["itemAvailableQuantity"]);
+    $dir = dirname(__FILE__);
+    if(isset($_FILES)) {
+        foreach($_FILES["files"]["tmp_name"] as $key => $tmpName){
+            $fileName = $_FILES["files"]["name"][$key];
+            $fileTmp = $_FILES["files"]["tmp_name"][$key];
+            $uploadDir = $dir . "/photos/" . $fileName;
+            move_uploaded_file($tmpName,$uploadDir);
+            $itemId = $newItem->getId();
+            $newItem->addPhoto($itemId, $uploadDir);
+        }
+        echo "pliki przeniesione <br>";
+    }
+    else {
+        echo "wystapil blad przy przenoszeniu <br>";
+    }
+
     if($newItem != false){
         echo "udalo sie dodac produkt";
     }
@@ -20,10 +36,12 @@ if($_SERVER["REQUEST_METHOD"] === "POST") {
 ?>
 
 
-<form action="" method="post">
+<form action="" enctype="multipart/form-data" method="post">
     <input type="text" id="itemName" name="itemName" placeholder="Name of Item">
     <input type="text" id="itemPrice" name="itemPrice" placeholder="Price of Item">
     <input type="text" id="itemDescription" name="itemDescription" placeholder="Description of Item">
     <input type="text" id="itemAvailableQuantity" name="itemAvailableQuantity" placeholder="Quantity of Item">
-    <input type="submit" id="submit">
+    <br>
+    <input multiple="true" type="file" name="files[]" id="uploadFile">
+    <input type="submit" id="submit" value="send">
 </form>
