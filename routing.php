a65541ca18f1d70b08d6bace0217dc6227ff447f
Filip-Konkowski <fkonkowski@gmail.com>
2015-10-24 21:59:11 +0200

    added new maps to router

--- routing.php
+++ routing.php
@@ -1,3 +1,8 @@
 <?php
 
-$router->map("GET", "/register/", "register.php");
+$router->map("GET", "register/", "registerHTML.php", "registerFile");
+$router->map("POST", "register/", "register.php", "registerFilePost");
+$router->map("GET", "login/", "login.php");
+$router->map("GET", "viewAllItems/", "viewAllItems.php", "viewAllItemsFile");
+$router->map("GET", "newItems/", "addItem.php", "addItemsFile");
+$router->map("POST", "newItems/", "addItem.php", "addItemsFilePost");
