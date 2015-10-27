18d9ef732b47d329b9bb86beb0f7e822262d5299
Filip-Konkowski <fkonkowski@gmail.com>
2015-10-27 11:28:10 +0100

    added data validation if form

--- register.php
+++ register.php
@@ -7,9 +7,23 @@ $error = [];
 if($_SERVER["REQUEST_METHOD"] === "POST") {
     if (isset($_POST["email"]) && strlen(trim($_POST["email"])) > 1 && filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
         $email = trim($_POST["email"]);
-    }
-    else {
+    } else {
         $error["email"] = true;
+    }
+    if (isset($_POST["name"]) && ctype_alpha($_POST["name"]) && strlen(trim($_POST["name"])) > 1){
+        $name = trim($_POST["name"]);
+    } else {
+        $error["name"] = true;
+    }
+    if (isset($_POST["surname"]) && ctype_alpha($_POST["surname"]) && strlen(trim($_POST["surname"])) > 1){
+        $name = trim($_POST["surname"]);
+    } else {
+        $error["surname"] = true;
+    }
+    if (isset($_POST["address"]) && strlen(trim($_POST["address"])) > 1){
+        $name = trim($_POST["address"]);
+    } else {
+        $error["address"] = true;
     }
 
     if(count($error) == 0) {
@@ -26,6 +40,15 @@ if($_SERVER["REQUEST_METHOD"] === "POST") {
 
 if (isset($error["email"])) {
     echo ("Uncorrect email address");
+}
+if (isset($error["name"])) {
+    echo ("Name needs to be at least one letter long. Names with numbers are uncorrect");
+}
+if (isset($error["surname"])) {
+    echo ("Surname needs to be at least one letter long. Surnames with numbers are uncorrect");
+}
+if (isset($error["address"])) {
+    echo ("Address needs to be at least one letter long. Address with numbers are uncorrect");
 }
 
 ?>
