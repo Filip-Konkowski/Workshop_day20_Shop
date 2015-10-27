6f85e4e1390b30a59dbaf793b81e7fd7ae0bad30
Filip-Konkowski <fkonkowski@gmail.com>
2015-10-27 11:01:43 +0100

    changes in reouting

--- routing.php
+++ routing.php
@@ -1,8 +1,11 @@
 <?php
 
+//$router->map("GET", "", "index.php", "indexFile");
+
 $router->map("GET", "register/", "registerHTML.php", "registerFile");
 $router->map("POST", "register/", "register.php", "registerFilePost");
 $router->map("GET", "login/", "login.php");
+
 $router->map("GET", "viewAllItems/", "viewAllItems.php", "viewAllItemsFile");
 $router->map("GET", "newItems/", "addItem.php", "addItemsFile");
 $router->map("POST", "newItems/", "addItem.php", "addItemsFilePost");
